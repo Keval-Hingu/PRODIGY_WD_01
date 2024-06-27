33564d9675c58fedb3446f853db5b3e704171c95
Keval-Hingu <160327086+Keval-Hingu@users.noreply.github.com>
2024-06-27 16:03:51 +0530

    Add files via upload

---
+++ task_01.js
@@ -0,0 +1,21 @@
+
+
+const about_section =  document.getElementById("About");
+const service_section =  document.getElementById("services")
+const form_section = document.getElementById("form");
+
+function About()
+{
+    about_section.scrollIntoView({behavior:"smooth"});
+}
+
+function form()
+{
+    form_section.scrollIntoView({behavior:"smooth"});
+}
+
+function service()
+{
+    service_section.scrollIntoView({behavior:"smooth"});
+
+}
